bb8bbab717d2987952d31c177fb8f973c608c5c4
Melansia <26363056+Melansia@users.noreply.github.com>
2025-01-19 22:29:46 +0200

    created the add,subtract,multiply,divide functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-alert("It's Alive!")
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const division = (a, b) => b === 0 ? a / b : "Division by zero!";
+
